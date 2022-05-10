4a50a3f8214d5b6344a9416e3133b9fa40b43d7b
Thor <thorck@protonmail.com>
2022-05-10 16:15:46 -0700

    aliases

--- 3-lagrange/src/lib.rs
+++ 3-lagrange/src/lib.rs
@@ -1,30 +1,53 @@
+// placeholder type aliases
+type Bit= u8;
+type Bitvec = Vec<Bit>;
+type Field = usize;
+type Rvec = Vec<usize>;
 
-fn compute_ml_ext<F>(v: Vec<usize>)-> F
-where F: Fn(Vec<usize>) -> usize{
+fn compute_ml_ext<F>(v: Bitvec) -> F
+where
+    F: Fn(Bitvec) -> usize,
+{
     todo!();
 }
 
-fn lemma_37(){
-    todo!();
-
-}
-
-fn lemma_38(){
+fn lagrange(w: Bitvec, r: Rvec){
     todo!();
 }
 
 
-fn main(){
+/// Compute f^~(r) in O(n log n) time and O(log n) space with a streaming pass
+fn lemma_37<F>(f: F, r: Rvec)
+where
+    F: Fn(Bitvec) -> Bit,
+{
+    let mut f_out = |x:Bitvec| 0;
+    for bit in r{
+        f_out = f_out(r)+ f(bit)*lagrange(w,r)
 
+    }
+    todo!();
 }
 
+fn lemma_38<F>(f: F, r: Bitvec)
+where
+    F: Fn(Bitvec) -> Bit,
+{
+    todo!();
+}
+
+fn main() {}
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
-#[test]
-fn test_compute_ml_ext(){
-    todo!()
-}
+    #[test]
+    fn test_compute_ml_ext() {
+        todo!()
+    }
+
+    fn test_exercise_34(){
+        todo!()
+    }
 }
