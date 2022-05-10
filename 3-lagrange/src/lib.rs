a0c0403a8b74df0dbdb4b0d1b6a2bbbec2c2939d
Thor <thorck@protonmail.com>
2022-05-10 15:58:54 -0700

    frame

---
+++ 3-lagrange/src/lib.rs
@@ -0,0 +1,30 @@
+
+fn compute_ml_ext<F>(v: Vec<usize>)-> F
+where F: Fn(Vec<usize>) -> usize{
+    todo!();
+}
+
+fn lemma_37(){
+    todo!();
+
+}
+
+fn lemma_38(){
+    todo!();
+}
+
+
+fn main(){
+
+}
+
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+#[test]
+fn test_compute_ml_ext(){
+    todo!()
+}
+}
